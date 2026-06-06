7fb010973a86c71e72eed23746437a6afc1946f4
Caramel Digital Studio <carameldigitalcontent@gmail.com>
2026-06-06 11:15:14 +0000

    Phase 2: Consolidate analysis libraries with unified interface

--- lib/analysis/index.ts
+++ lib/analysis/index.ts
@@ -10,3 +10,28 @@
 export * from './entropy-analysis';
 export * from './correlation-analysis';
 export * from './information-bottleneck';
+
+/**
+ * Run complete analysis suite on state vector
+ */
+export async function runFullAnalysis(stateVector: number[]) {
+  const { calculateShannonEntropy, getNormalizedEntropy, calculateComplexityMetric } = await import('./entropy-analysis');
+  const { calculatePearsonCorrelation, calculateNetworkDensity } = await import('./correlation-analysis');
+  const { identifyInformationBottleneck, calculatePredictability } = await import('./information-bottleneck');
+
+  return {
+    entropy: {
+      shannon: calculateShannonEntropy(stateVector),
+      normalized: getNormalizedEntropy(stateVector),
+      complexity: calculateComplexityMetric(stateVector),
+    },
+    correlation: {
+      pearson: calculatePearsonCorrelation(stateVector, stateVector),
+      networkDensity: calculateNetworkDensity(stateVector),
+    },
+    bottleneck: {
+      identified: identifyInformationBottleneck(stateVector),
+      predictability: calculatePredictability(stateVector),
+    },
+  };
+}
